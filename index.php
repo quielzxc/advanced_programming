68ac481b70ec329d2c57fc78412b65f743dc388e
quielzxc <quieljeremiahcariaso04@gmail.com>
2020-11-06 15:33:14 +0800

    index of assign_two

--- index.php
+++ index.php
@@ -1,111 +1,44 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<?php
+class User{
+        public $name;
+        public $username;
+        public $email;
 
-<div class="container">
-  <div class="row">
-    <div class="col" style="padding-top: 100px;">
-      <h1>Quiel Jeremiah L. Cariaso</h1>
-    </div>
-    <div class="col">
-      <form class="" action="" method="post" style="padding-left: 100px;">
-          <h3>CALCULATOR</h3>
-          <p>Input the First Number</p>
-          <input type="number" name="input1">
+      function set_name($name){
+        $this->name = $name;
+      }
+      function get_name(){
+        return $this->name;
+      }
+      function set_username($username){
+        $this->username = $username;
+      }
+      function get_username(){
+        return $this->username;
+      }
+      function set_email($email){
+        $this->email = $email;
+      }
+      function get_email(){
+        return $this->email;
+      }
 
-          <p>Input the Second Number</p>
-          <input type="number" name="input2">
-          <br>
-          <br>
-          <button type="submit" name="add" value="add"> + </button>
-          <button type="submit" name="subtract" value="subtract"> - </button>
-          <button type="submit" name="multi" value="multi"> * </button>
-          <button type="submit" name="divide" value="divide"> / </button>
-          <p>Answer</p>
-          <textarea name="name" rows="1" cols="30">
-              <?php
-                if (isset($_POST['add'])) {
-                  $num1 = $_POST['input1'];
-                  $num2 = $_POST['input2'];
-                  $result = $num1 + $num2;
-                  echo $result;
-                }
-                if (isset($_POST['subtract'])) {
-                  $num1 = $_POST['input1'];
-                  $num2 = $_POST['input2'];
-                  $result = $num1 - $num2;
-                  echo $result;
-                }
-                if (isset($_POST['multi'])) {
-                  $num1 = $_POST['input1'];
-                  $num2 = $_POST['input2'];
-                  $result = $num1 * $num2;
-                  echo $result;
-                }
-                if (isset($_POST['divide'])) {
-                  $num1 = $_POST['input1'];
-                  $num2 = $_POST['input2'];
-                  $result = $num1 / $num2;
-                  echo $result;
-                }
+      function addFriend($name){
+        echo $name . " added a friend <br>";
+      }
+      function postStatus($name){
+        echo $name . " posted a status <br>";
+      }
+    }
 
-               ?>
-          </textarea>
+      $example = new User();
+      $example->set_name("Quiel Jeremiah L. Cariaso");
+      $example->set_email("quieljeremiahcariaso04@gmail.com");
+      $example->set_username("quieljeremiahcariaso");
 
-        </form>
-    </div>
-    <div class="col">
-      <form class="" action="" method="post">
-        <h3>Finding GCD</h3>
-        <p>Input the First Number</p>
-        <input type="number" name="first" value="first">
-        <p>Input the Second Number</p>
-        <input type="number" name="second" value="first">
-        <br>
-        <br>
-        <button type="submit" name="submit" value="submit">Submit</button>
-        <p>Answer</p>
-        <textarea name="name" rows="1" cols="20">
-          <?php
-            if (isset($_POST['submit'])) {
-              $num1 = $_POST['first'];
-              $num2 = $_POST['second'];
-
-              if ($num1 > $num2)
-              {
-                $temp = $num1;
-                $num1 = $num2;
-                $num2 = $temp;
-              }
-              for($i = 1; $i < ($num1+1); $i++)
-              {
-                if ($num1%$i == 0 and $num2%$i == 0)
-                $gcd = $i;
-              }
-              echo $gcd;
-            }
-          ?>
-        </textarea>
-      </form>
-    </div>
-  </div>
-</div>
-
-<style media="screen">
-  .col{
-    float: left;
-    width: 33.33%;
-  }
-  .row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-</style>
-
-  </body>
-</html>
+      echo "Name: " . $example->get_name() . "<br>";
+      echo "Username: " . $example->get_username() . "<br>";
+      echo "Email: " . $example->get_email() . "<br>";
+      $example->addFriend("Quiel");
+      $example->postStatus("Quiel");
+?>
